064e3dda25856b37534a12197679566e10fdbd8a
marcelvieira <marcelfonteles@gmail.com>
2019-08-26 10:00:39 -0300

    working slices 2

---
+++ workingSlices2/workingSlices2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var slice1 []int
+	slice1 = append(slice1, 1)
+	fmt.Println(slice1)
+	slice1 = append(slice1, 2)
+	fmt.Println(slice1)
+	slice1 = append(slice1, slice1...)
+	fmt.Println(slice1)
+
+	var slice2 = []int{1,2,3,4,5,6,7,8,9}
+	fmt.Println(slice2)
+
+	slice2 = append(slice2[:4], slice2[6:]...)
+	fmt.Println(slice2)
+}
